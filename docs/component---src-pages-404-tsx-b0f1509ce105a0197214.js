2ec5b0682a87cc8de4d9100b3c7645e7205db2ff
Fujimiya Nevermore <donald33wang@gmail.com>
2021-04-06 21:19:37 +0800

    Build

---
+++ docs/component---src-pages-404-tsx-b0f1509ce105a0197214.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[218],{938:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(4233),s=a(9470);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{data:e,location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-b0f1509ce105a0197214.js.map
